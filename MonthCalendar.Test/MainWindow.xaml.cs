ed5f98c8484c264fab2d72d235c9815bbd0d72c7
Patrik Bohman <patrik.bohman@hotmail.com>
2022-03-15 14:07:06 +0100

    Improved test client

--- MonthCalendar.Test/MainWindow.xaml.cs
+++ MonthCalendar.Test/MainWindow.xaml.cs
@@ -53,53 +53,33 @@ namespace MonthCalendar.Test
     private void Setup()
     {
 
+      var year = DateTime.Now.Year;
+      var month = DateTime.Now.Month;
 
-      this.days.Add(new Day() { Date = new DateTime(2020, 6, 6), BackgroundColor = Colors.ForestGreen, Text = "HEJ HEJ" });
-      this.days.Add(new Day() { Date = new DateTime(2020, 10, 21), BackgroundColor = Colors.BlueViolet });
-      this.days.Add(new Day() { Date = new DateTime(2020, 10, 11), BackgroundColor = Colors.Orange });
+      this.MyCalendar.Year = year;
+      this.MyCalendar.Month = month;
+      
+      this.days.Add(new Day() { Date = new DateTime(year, month, 6), BackgroundColor = Colors.LightGreen, Text = "Dentist",
+        Image = new BitmapImage(new Uri("pack://application:,,,/Resources/star.png", UriKind.Absolute))
+      });
+      this.days.Add(new Day() { Date = new DateTime(year, month, 21), Text = "Party!!",
+        Image = new BitmapImage(new Uri("pack://application:,,,/Resources/present.png", UriKind.Absolute))
+      });
+      this.days.Add(new Day() { Date = new DateTime(year, month, 11), BackgroundColor = Colors.Orange, Text = "Pizza", 
+        Image = new BitmapImage(new Uri("pack://application:,,,/Resources/pizza.png", UriKind.Absolute)) });
 
-      this.days.Add(new Day()
-      {
-        Date = new DateTime(2020, 8, 13),
-        BackgroundColor = Colors.HotPink,
-        DateColor = Colors.Yellow,
-        Image = new BitmapImage(new Uri("pack://application:,,,/Resources/star.png", UriKind.Absolute)),
-        DateFontSize = 22,
-        Text = "BIDEN",
-        Tooltip = "This is a test.",
-        TextColor = Colors.White,
-        TextFontSize = 36//, //,
-        //Template = (DataTemplate)Application.Current.FindResource("myDayTemplate2")
-    });
-    /*  this.days.Add(new Day() { Date = new DateTime(2020, 8, 16), Tooltip = "Make america great again!", 
-        BackgroundColor = Colors.Linen, DateColor = Colors.DarkOrange, Text = "TRUMP", TextColor = Colors.Ivory, TextFontSize = 22 });
+      this.days.Add(new Day() { Date = new DateTime(year, month, 16), BackgroundColor = Colors.LightYellow, Text = "Match",
+        Image = new BitmapImage(new Uri("pack://application:,,,/Resources/soccer.png", UriKind.Absolute))
+      });
 
-      this.days.Add(new Day() { Date = new DateTime(2020, 7, 28), DateColor = Colors.Red, Text = "yyy" });*/
-
+      this.DisabledDays = new ObservableCollection<DateTime>() { new DateTime(year, month, 25), new DateTime(year, month, 26) };
 
       this.weeks.Add(new Week() { TextColor = Colors.White, BackgroundColor = Colors.Green, TextFontWeight = FontWeights.Bold, 
-        Number = 32, Year = 2020, Tooltip = "Denna vecka är den först i ...." });
+         Number = 32, Year = year });
 
-        this.weekdays.Add(new Weekday() { /*Template = (DataTemplate)Application.Current.FindResource("myWeekdayTemplate"),*/
-          BackgroundColor = Colors.Orange,
-          Tooltip = "Detta är en veckodag.",
-          TextColor = Colors.White, TextFontSize = 16, TextFontWeight = FontWeights.Bold, Year = 2020, Month = 8, DayOfWeek = DayOfWeek.Monday });
+      this.weekdays.Add(new Weekday() { BackgroundColor = Colors.Wheat, 
+        TextColor = Colors.White, TextFontSize = 16, TextFontWeight = FontWeights.Bold, Year = year, Month = month, DayOfWeek = DayOfWeek.Tuesday });
 
-      //this.weeknumberProperties.TextColor = Colors.DarkOliveGreen;
-      //this.weeknumberProperties.FontSize = 32;
-      //this.weeknumberProperties.FontWeight = FontWeights.Bold;
-      //this.weeknumberProperties.TextDecoration = "Underline";
-      //this.weeknumberProperties.BackgroundColor = Colors.LightBlue;
-      //this.headerProperties.BackgroundColor = Colors.Lavender;
-      this.weekdaysProperties.BackgroundColor = Colors.DarkSeaGreen;
-      //this.weekdaysProperties.FontWeight = FontWeights.Bold;
-      //this.weekdaysProperties.TextDecoration = "Strikethrough";
-      //this.weekdaysProperties.TextColor = Colors.Firebrick;
-      //this.weekdaysProperties.FontSize = 30;
-      //this.HeaderProperties.DateText = "jjj"; // .Text = "TEST TEST TEST";
-
-      //this.weekdaysProperties.AbbreviatedNames = false;
-       
       this.calendarProperties.ShowNotCurrentMonth = true;
 
       this.calendarProperties.DateFontSize = 24;
@@ -108,10 +88,8 @@ namespace MonthCalendar.Test
       this.calendarProperties.DateTextDecoration = "Underline";
       this.calendarProperties.DateFontStyle = FontStyles.Italic;
       this.calendarProperties.DateFontWeight = FontWeights.Bold;
-      this.MonthProperties.SelectedBackgroundColor = Colors.Red;
-      this.calendarProperties.SelectedBackgroundColor = Colors.Red;
-
-      //this.calendarProperties.BackgroundImage = new BitmapImage(new Uri("pack://application:,,,/Resources/bricks.jpeg", UriKind.Absolute));
+      this.MonthProperties.SelectedBackgroundColor = Colors.LightBlue;
+      this.calendarProperties.SelectedBackgroundColor = Colors.LightBlue;
 
       this.calendarProperties.NotCurrentMonthBackgroundColor = Colors.Gray;
       this.calendarProperties.NotCurrentMonthDateColor = Colors.White;
@@ -119,18 +97,17 @@ namespace MonthCalendar.Test
       this.monthProperties.AbbreviatedNames = true;
       this.calendarProperties.TextColor = Colors.Pink;
 
-      this.days.Add(new Day() { Date = new DateTime(2020, 7, 28), DateColor = Colors.Red, Text = "yyy" });
-
       this.months.Add(new Month()
       {
         Number = 10,
-        Year = 2021,
+        Year = year,
+        BackgroundColor = Colors.Orchid,
         Image = new BitmapImage(new Uri("pack://application:,,,/Resources/star.png", UriKind.Absolute)),
       });
       this.months.Add(new Month()
       {
         Number = 6,
-        Year = 2021,
+        Year = year,
         BackgroundColor = Colors.Beige
       });
 
@@ -197,8 +174,6 @@ namespace MonthCalendar.Test
       }
     }
 
-  
-
     public FooterProperties FooterProperties => footerProperties;
 
     public MonthProperties MonthProperties => monthProperties;
@@ -210,54 +185,7 @@ namespace MonthCalendar.Test
     private void MonthCalendar_MonthChanged(object sender, Pabo.MonthCalendar.EventArgs.MonthChangedEventArgs e)
     {
     }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-      /*this.headerProperties.Text = "Uj UJ UJ";
-      var day = this.days.FirstOrDefault(x => x.Date == new DateTime(2020, 8, 13));
-      if (day != null)
-      {
-        day.TextColor = Colors.Blue;
-      }
-      var week = this.weeks.FirstOrDefault(x => x.Number == 32);
-      if (day != null)
-      {
-        week.TextColor = Colors.Red;
-      }
-
-      MyCalendar.SuspendLayout();*/
-
-      //this.WeeknumberProperties.TextColor = Colors.Purple;
-      //this.WeeknumberProperties.TextFontSize = 24;
-      //this.WeeknumberProperties.TextFontWeight = FontWeights.Bold;
-      //this.WeekdaysProperties.TextColor = Colors.Purple;
-      //this.WeekdaysProperties.TextFontSize = 24;
-      //this.WeekdaysProperties.TextFontWeight = FontWeights.Bold;
-
-      //this.CalendarProperties.DateColor = Colors.Blue;
-      //this.CalendarProperties.DateFontSize = 10;
-
-      //MyCalendar.ResumeLayout();
-
-      //this.MyCalendar.Refresh();
-
-      //MyCalendar.Select(new List<DateTime>() { new DateTime(2020, 8, 7), new DateTime(2020, 8, 8) });
-      //MyCalendar.Select(new List<DateTime>() { new DateTime(2020, 8, 15), new DateTime(2020, 8, 16) });
-      //MyCalendar.Deselect(new List<DateTime>() { new DateTime(2020, 8, 7), new DateTime(2020, 8, 8) });
-
-      //MyCalendar.SelectWeek(34);
-      //MyCalendar.DeselectWeek(34);
-
-      //MyCalendar.SelectWeekday(DayOfWeek.Thursday);
-      //MyCalendar.DeselectWeekday(DayOfWeek.Thursday);
-
-      //MyCalendar.MinDate = new DateTime(2020, 8, 12);
-
-      //this.MinDate = new DateTime(2020, 8, 16);
-
-      this.DisabledDays = new ObservableCollection<DateTime>() { new DateTime(2020, 8, 12), new DateTime(2020, 8, 16) };
-
-    }
+       
 
     public event PropertyChangedEventHandler PropertyChanged;
 
@@ -284,12 +212,10 @@ namespace MonthCalendar.Test
 
     private void MonthCalendar_WeekdayLeave(object sender, Pabo.MonthCalendar.EventArgs.WeekdayEventArgs e)
     {
-      //Debug.WriteLine("Leave=" + e.Weekday.DayOfWeek.ToString());
     }
 
     private void MonthCalendar_WeekdayEnter(object sender, Pabo.MonthCalendar.EventArgs.WeekdayEventArgs e)
     {
-      //Debug.WriteLine("Enter=" + e.Weekday.DayOfWeek.ToString());
     }
 
     private void MonthCalendar_FooterEnter(object sender, RoutedEventArgs e)
@@ -336,6 +262,11 @@ namespace MonthCalendar.Test
     {
     }
 
-    
+    private void RadioButton_Checked(object sender, RoutedEventArgs e)
+    {
+        this.MyCalendar.VisualMode = sender == mode1
+        ? Pabo.MonthCalendar.Common.VisualMode.Days 
+        : Pabo.MonthCalendar.Common.VisualMode.Months;
+    }
   }
 }
